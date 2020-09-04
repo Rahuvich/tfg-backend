9b1db15bae31f89d3632195cd2d9de053df216ef
Raul <Mabe>
2020-09-04 12:00:44 +0200

    cd

--- src/index.js
+++ src/index.js
@@ -43,17 +43,6 @@ const server = new ApolloServer({
 
 app.use(HttpAuth);
 
-app.use(function (req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect("https://" + req.headers.host + req.url);
-  }
-});
-app.enable("trust proxy");
-
 server.applyMiddleware({ app });
 
 const httpServer = createServer(app);
